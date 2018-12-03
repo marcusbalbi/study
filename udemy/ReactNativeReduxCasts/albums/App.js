ca1d7011018224dd571baa08f6ac84516fcb6b7a
Marcus Balbi <marcus.balbi@rgsistemas.com.br>
2018-12-03 15:19:15 -0200

    axios loading albums data

--- udemy/ReactNativeReduxCasts/albums/App.js
+++ udemy/ReactNativeReduxCasts/albums/App.js
@@ -3,7 +3,6 @@ import { View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
-// http://rallycoding.herokuapp.com/api/music_albums
 export default () => (
   <View>
     <Header title={'Albums'} />
--- udemy/ReactNativeReduxCasts/albums/src/components/AlbumList.js
+++ udemy/ReactNativeReduxCasts/albums/src/components/AlbumList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  }
   componentWillMount() {
-    console.log('component will mount on list');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(({ data }) => this.setState({ albums: data }));
   }
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>AlbumList</Text>

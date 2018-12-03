b2336916f3a9879509f70499b41a8f4773d8b014
Marcus Balbi <marcus.balbi@rgsistemas.com.br>
2018-12-03 11:02:44 -0200

    primeiro commit react-native albums

---
+++ udemy/ReactNativeReduxCasts/albums/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+
+export default () => (
+  <View>
+    <Header />
+  </View>
+);
---
+++ udemy/ReactNativeReduxCasts/albums/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 20
+  },
+  container: {
+    // heigth: 60,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: 'grey',
+    shadowOpacity: 1,
+    position: 'relative'
+  }
+});
+
+export default class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text style={styles.titleStyle} >Albums</Text>
+      </View>
+    );
+  }
+}

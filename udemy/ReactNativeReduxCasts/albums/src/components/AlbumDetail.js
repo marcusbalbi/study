d480970c417d180d8981760c3382b603d8587410
Marcus Balbi <marcus.balbi@rgsistemas.com.br>
2018-12-04 13:56:10 -0200

    Ambum Detail styles

--- udemy/ReactNativeReduxCasts/albums/src/components/AlbumDetail.js
+++ udemy/ReactNativeReduxCasts/albums/src/components/AlbumDetail.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-export default (props) => (
-  <Card>
-    <CardSection>
-        <Text>{props.album.title}</Text>
-    </CardSection>
-  </Card>
+export default ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={styles.imageContainer}>
+          <Image style={styles.thumbnail} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  imageContainer: {
+
+  },
+  thumbnail: {
+    width: 50,
+    height: 50
+  },
+  infoContainer: {
+    justifyContent: 'space-around',
+    flex: 1
+  }
+};

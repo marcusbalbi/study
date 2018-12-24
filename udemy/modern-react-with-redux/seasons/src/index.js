c4859abe4c1b7d397115bff3032b0001c0598e5b
Marcus Balbi <balbimarcus@gmail.com>
2018-12-24 16:34:01 -0200

    working w states

--- udemy/modern-react-with-redux/seasons/src/index.js
+++ udemy/modern-react-with-redux/seasons/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Root extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {lat: null, errorMessage: null}
+    window.navigator.geolocation.getCurrentPosition(
+      ({coords}) => {
+        this.setState({lat: coords.latitude})
+      },
+      ({message}) => { 
+        this.setState({errorMessage: message}) 
+      }
+    )
+  }
+  render () {
+    if (this.state.lat) {
+      return (
+        <div>
+          latitude: {this.state.lat}
+        </div>
+      )
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    if (this.state.errorMessage) {
+      return <div>error: {this.state.errorMessage}</div>
+    }
+
+    return <div>Loading...</div>
+  }
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'))

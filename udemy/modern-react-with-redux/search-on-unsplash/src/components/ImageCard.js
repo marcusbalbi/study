533ab3eb1e79dee26aaff6590b4094d8e400606d
Marcus Balbi <balbimarcus@gmail.com>
2019-02-05 21:07:43 -0200

    unsplash search example done

--- udemy/modern-react-with-redux/search-on-unsplash/src/components/ImageCard.js
+++ udemy/modern-react-with-redux/search-on-unsplash/src/components/ImageCard.js
@@ -3,17 +3,23 @@ import React from 'react'
 class ImageCard extends React.Component {
   constructor (props) {
     super(props)
-
+    this.state = { spans: 0 }
     this.imageRef = React.createRef()
   }
 
   componentDidMount () {
-    console.log(this.imageRef)
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+
+  setSpans  = () => {
+    const height = this.imageRef.current.clientHeight
+    const spans = Math.ceil(height / 10)
+    this.setState({ spans })
   }
   render () {
     let { description, urls } = this.props.image
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }} >
         <img ref={this.imageRef} src={urls.regular} alt={description}  />
       </div>
     )

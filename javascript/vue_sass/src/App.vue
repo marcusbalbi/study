93472a04ddbe85f08c37e88cbbd543f010dfb989
Marcus Balbi <balbimarcus@gmail.com>
2019-04-01 19:12:22 -0300

    tst vue with sass hot reload

---
+++ javascript/vue_sass/src/App.vue
@@ -0,0 +1,14 @@
+<style src="./assets/style/main.scss" lang="scss"></style>
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <button class="btn">TASK</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>

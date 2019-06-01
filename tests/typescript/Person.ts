373f2073548c440377e7cf19f00a4958fbfc75dc
Marcus Balbi <balbimarcus@gmail.com>
2019-06-01 17:54:43 -0300

    scroll progress and first steps into typescripts

---
+++ tests/typescript/Person.ts
@@ -0,0 +1,10 @@
+
+class Person {
+  private name: String
+  private age: Number
+  constructor(name: String, age: Number) {
+    this.name = name
+    this.age = age
+  }
+}
+export default Person
---
+++ tests/typescript/index.ts
@@ -0,0 +1,5 @@
+import Person from './Person'
+
+let a = new Person('American', 12)
+
+console.log(a)

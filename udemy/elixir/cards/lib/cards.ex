1ff2d088297ff390f3c4d31b24f70c0f9a1aada8
Marcus Balbi <balbimarcus@gmail.com>
2019-06-10 21:56:00 -0300

    working with files

--- udemy/elixir/cards/lib/cards.ex
+++ udemy/elixir/cards/lib/cards.ex
@@ -14,4 +14,22 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, nCards) do
+    Enum.split(deck, nCards)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+    binary
+  end
+
+  def load(filename) do
+    { status, binary } = File.read(filename)
+    case status do
+     :ok -> :erlang.binary_to_term(binary)
+     :error -> "Arquivo Inexistente" 
+    end
+  end
 end

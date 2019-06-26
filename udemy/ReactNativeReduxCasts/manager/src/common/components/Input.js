7b2c0d0e9fb41098d94077a52f08458542b040e1
Marcus Balbi <balbimarcus@gmail.com>
2019-06-25 22:40:20 -0300

    creating an INput Component

---
+++ udemy/ReactNativeReduxCasts/manager/src/common/components/Input.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, TextInput, Text, StyleSheet } from 'react-native'
+
+const Input = props => {
+  return (
+    <View style={Styles.container} >
+      <Text style={Styles.text} >{props.label}</Text>
+      <TextInput
+        secureTextEntry={props.hide}
+        style={Styles.input}
+        onChangeText={props.onTextChange}
+        value={props.value} />
+    </View>
+  )
+}
+
+const Styles = StyleSheet.create({
+  text: {
+    fontSize: 26,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 5
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#CCC',
+    fontSize: 22,
+    paddingLeft: 10
+  },
+  container: {
+    marginBottom: 10
+  }
+})
+
+Input.defaultProps = {
+  hide: false,
+  value: '',
+  label: 'Text',
+  onTextChange: () => {}
+}
+
+export default Input

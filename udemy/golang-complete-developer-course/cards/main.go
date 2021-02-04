3019043e347c98feabf671ae2361e41421f11d95
Marcus Balbi <balbimarcus@gmail.com>
2021-02-04 18:24:39 -0300

    creating type

--- udemy/golang-complete-developer-course/cards/main.go
+++ udemy/golang-complete-developer-course/cards/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of spades"
-	card = "Five of diamonds"
-	fmt.Println(card)
+	// card := "Ace of spades"
+	// card = "Five of diamonds"
+	// card := newCard()
+	// fmt.Println(card)
+
+	// cards := []string{newCard(), "Ace of spades"}
+	cards := deck{newCard(), "Ace of spades"}
+	cards = append(cards, "two of hearts")
+	cards.print()
+
+}
+
+func newCard() string {
+	return "five of Diamonds"
 }

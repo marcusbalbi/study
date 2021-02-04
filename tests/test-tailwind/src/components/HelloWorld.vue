91141128f862c3d53ad58cbdcd58d6048088e34c
Marcus Balbi <balbimarcus@gmail.com>
2021-02-04 16:31:33 -0300

    tests on tailwind

---
+++ tests/test-tailwind/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-12">
+      <button class="btn bg-indigo-500 hover:bg-indigo-400 sm:col-span-2 lg:col-span-2" >Botão Simples</button>
+      <button class="btn btn-primary sm:col-span-2 lg:col-span-2" >Botão Simples</button>
+      <button class="btn btn-primary sm:col-span-4 lg:col-span-2" >Botão Simples</button>
+      <button class="btn btn-primary sm:col-span-2 lg:col-span-2" >Botão Simples</button>
+      <button class="btn btn-primary sm:col-span-2 lg:col-span-4" >Botão Simples</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style>
+.main-title {
+}
+</style>

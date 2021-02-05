a5ee6382a6b3ebf222d71be281fbb8d11beb5552
Marcus Balbi <balbimarcus@gmail.com>
2021-02-05 17:57:08 -0300

    another tests

--- udemy/golang-complete-developer-course/cards/deck_test.go
+++ udemy/golang-complete-developer-course/cards/deck_test.go
@@ -7,4 +7,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("The first card of deck should be Ace of Spades got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("The last card of deck should be King of Clubs got %v", d[len(d)-1])
+	}
 }

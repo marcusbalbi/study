d0975e4a2c0c59c4a606d5eeeb8839230477cf54
Marcus Balbi <balbimarcus@gmail.com>
2021-02-07 12:14:54 -0300

    set channel

--- udemy/golang-complete-developer-course/channes/main.go
+++ udemy/golang-complete-developer-course/channes/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
@@ -16,18 +15,22 @@ func main() {
 		"http://invalidsite.xpto.optp",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
 
-	time.Sleep(time.Second * 5)
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " might be down")
+		c <- "might be down"
 		return
 	}
+	c <- "is up!"
 	fmt.Println(link, "is up!")
 }

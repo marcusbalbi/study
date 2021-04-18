72b098193a3211aee1b0d0936b9161a5799f5411
Marcus Balbi <balbimarcus@gmail.com>
2021-04-18 17:31:07 -0300

    finish accordion basic

--- udemy/modern-react-with-redux/widgets/src/components/Accordion.js
+++ udemy/modern-react-with-redux/widgets/src/components/Accordion.js
@@ -2,27 +2,32 @@ import React from "react";
 import useItems from "../hooks/useItems";
 
 const Accordion = () => {
-  const {items, selectedItem, setSelectedItem} = useItems();
+  const { items, selectedItem, setSelectedItem } = useItems();
   const renderItems = () => {
     return items.map((item, i) => {
+      const active = i === selectedItem ? "active" : "";
       return (
-        <React.Fragment key={item.title} >
-          <div className="title active" onClick={() => { onTitleClicked(i); }}>
+        <React.Fragment key={item.title}>
+          <div
+            className={`title ${active}`}
+            onClick={() => {
+              onTitleClicked(i);
+            }}
+          >
             <i className="dropdown icon"></i>
-            {item.title} - {selectedItem === i ? "SELECIONADO" : ""}
+            {item.title}
           </div>
-          <div className="content active">
+          <div className={`content ${active}`}>
             <p>{item.content}</p>
           </div>
         </React.Fragment>
       );
-    })
-  }
+    });
+  };
   const onTitleClicked = (index) => {
     setSelectedItem(index);
-  }
+  };
   return <div className="ui styled accordion">{renderItems()}</div>;
-}
-
+};
 
 export default Accordion;

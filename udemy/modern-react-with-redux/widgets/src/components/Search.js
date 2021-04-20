d168e0c59e1a95dc044d22a85d9f90444c6f266a
Marcus Balbi <balbimarcus@gmail.com>
2021-04-20 19:05:15 -0300

    separate state to fix debounced

--- udemy/modern-react-with-redux/widgets/src/components/Search.js
+++ udemy/modern-react-with-redux/widgets/src/components/Search.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+
+      return () => {
+        clearTimeout(timerId);
+      };
+    }, 500);
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,25 +24,13 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-        return () => {
-          clearTimeout(timeoutId);
-        };
-      }, 500);
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((item) => {
     return (

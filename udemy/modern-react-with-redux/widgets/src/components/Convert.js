ff43ba26c1bbb745b5177f70f3ffd629a7883922
Marcus Balbi <balbimarcus@gmail.com>
2021-04-21 16:10:12 -0300

    Convert completed, need debounce

--- udemy/modern-react-with-redux/widgets/src/components/Convert.js
+++ udemy/modern-react-with-redux/widgets/src/components/Convert.js
@@ -1,27 +1,36 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 // URL: https://translation.googleapis.com/language/translate/v2
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState();
   useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        `https://translation.googleapis.com/language/translate/v2`,
+        {},
+        {
+          params: {
+            key: KEY,
+            target: language.value,
+            q: text,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
     if (!language || !text) {
       return;
     }
-    axios.post(
-      `https://translation.googleapis.com/language/translate/v2`,
-      {},
-      {
-        params: {
-          key: KEY,
-          target: language.value,
-          q: text,
-        },
-      }
-    );
+    doTranslation();
   }, [language, text]);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;

01b684b09cf2f21c00c59dc281ca73d59407032a
Marcus Balbi <balbimarcus@gmail.com>
2021-04-21 12:25:34 -0300

    add props to drowndown

--- udemy/modern-react-with-redux/widgets/src/App.js
+++ udemy/modern-react-with-redux/widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 // import Search from "./components/Search"
@@ -17,9 +17,13 @@ const options = [
   },
 ];
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <>
-      <Dropdown options={options} />
+      <Dropdown 
+        options={options}
+        onSelectedChange={(item) => { setSelected(item) }}
+        selected={selected} />
       {/* <Accordion /> */}
     </>
   );
--- udemy/modern-react-with-redux/widgets/src/components/Dropdown.js
+++ udemy/modern-react-with-redux/widgets/src/components/Dropdown.js
@@ -1,7 +1,9 @@
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className="item">
+      <div key={option.value} className="item" onClick={() => {
+        onSelectedChange(option)
+      }} >
         {option.label}
       </div>
     );
@@ -12,7 +14,7 @@ const Dropdown = ({ options }) => {
         <label className="label">Select a Color</label>
         <div className="ui selection dropdown visible active">
           <i className="dropdown icon"></i>
-          <div className="text">Select Color</div>
+          <div className="text">{selected.label}</div>
           <div className="menu visible transition">{renderedOptions}</div>
         </div>
       </div>

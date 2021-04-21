3971e88f8292e769ab531ece3c1947a0707285e7
Marcus Balbi <balbimarcus@gmail.com>
2021-04-21 12:41:16 -0300

    openclose dropdown

--- udemy/modern-react-with-redux/widgets/src/components/Dropdown.js
+++ udemy/modern-react-with-redux/widgets/src/components/Dropdown.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
   const renderedOptions = options.map((option) => {
     if (selected === option) {
       return null;
     }
     return (
-      <div key={option.value} className="item" onClick={() => {
-        onSelectedChange(option)
-      }} >
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onSelectedChange(option);
+        }}
+      >
         {option.label}
       </div>
     );
@@ -15,10 +22,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>

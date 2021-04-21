43a438c3a4a7d84339a07cc9a14c8fbb1d9a569a
Marcus Balbi <balbimarcus@gmail.com>
2021-04-21 15:05:13 -0300

    Convert component

---
+++ udemy/modern-react-with-redux/widgets/src/components/Convert.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const Convert = ({language, text}) => {
+
+  useEffect(() => {
+
+  }, [language, text])
+
+  return (
+    <div>
+      
+    </div>
+  )
+
+}
+
+
+export default Convert;
--- udemy/modern-react-with-redux/widgets/src/components/Translate.js
+++ udemy/modern-react-with-redux/widgets/src/components/Translate.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Convert from "./Convert";
 import Dropdown from "./Dropdown";
 
 const options = [
@@ -15,13 +16,30 @@ const options = [
     value: "hi",
   },
 ];
+// URL: https://translation.googleapis.com/language/translate/v2
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState();
   return (
     <div>
+      <div className="ui form">
+        <div className="field" >
+          <label>Enter Text</label>
+          <input
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+        <hr />
+        <h3 className="ui header" >Output</h3>
+        <Convert language={language} text={text} />
+      </div>
       <Dropdown
         selected={language}
+        label="Select a Language"
         onSelectedChange={setLanguage}
         options={options}
       />

24de080e608c29143e10557981fb57d46e39a4da
Marcus Balbi <balbimarcus@gmail.com>
2021-05-01 10:01:54 -0300

    start webpack project

---
+++ udemy/webpack/js_modules/src/index.js
@@ -0,0 +1,3 @@
+const sum = require("./sum");
+
+console.log(sum(10, 5));
---
+++ udemy/webpack/js_modules/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname+"/build",
+    filename: "bundle.js"
+  }
+}

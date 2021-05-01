6b7096b9ad5d6ca0db3341bcf94a877b250a374a
Marcus Balbi <balbimarcus@gmail.com>
2021-05-01 20:16:30 -0300

    load some image into html

---
+++ udemy/webpack/js_modules/src/image_viewer.js
@@ -0,0 +1,5 @@
+const image = document.createElement("img");
+
+image.src = "http://lorempixel.com.br/500/400/?1";
+
+document.body.appendChild(image);
--- udemy/webpack/js_modules/src/index.js
+++ udemy/webpack/js_modules/src/index.js
@@ -1,3 +1,4 @@
 import { sum } from "./sum";
+import "./image_viewer";
 
 console.log(sum(10, 5));

1756343bd68da10d644d143e900dc1784b7ad0a4
Marcus Balbi <balbimarcus@gmail.com>
2021-05-01 20:27:43 -0300

    playing with css loader

--- udemy/webpack/js_modules/src/image_viewer.js
+++ udemy/webpack/js_modules/src/image_viewer.js
@@ -1,3 +1,5 @@
+import "../styles/image_viewer.css";
+
 const image = document.createElement("img");
 
 image.src = "http://lorempixel.com.br/500/400/?1";
--- udemy/webpack/js_modules/webpack.config.js
+++ udemy/webpack/js_modules/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "none",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -16,6 +16,11 @@ module.exports = {
           loader: "babel-loader",
           options: { presets: ["@babel/preset-env"], targets: "defaults" },
         },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },

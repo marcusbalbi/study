4c22de887f8df3bf041dbaf4eea661690d903a93
Marcus Balbi <balbimarcus@gmail.com>
2021-05-02 12:20:22 -0300

    start loading dynamically

--- udemy/webpack/js_modules/src/index.js
+++ udemy/webpack/js_modules/src/index.js
@@ -1,4 +1,15 @@
-import { sum } from "./sum";
-import "./image_viewer";
+// import { sum } from "./sum";
+// import "./image_viewer";
 
-console.log(sum(10, 5));
+// console.log(sum(10, 5));
+
+
+const button = document.createElement("button");
+button.innerText = "Click me!"
+
+button.onclick = () => {
+  import()
+}
+
+
+document.body.appendChild(button);

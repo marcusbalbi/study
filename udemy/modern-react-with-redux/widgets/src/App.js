9cc73118abdcfb495ae869b0e1979803d20448d7
Marcus Balbi <balbimarcus@gmail.com>
2021-05-05 17:47:04 -0300

    basic routing

--- udemy/modern-react-with-redux/widgets/src/App.js
+++ udemy/modern-react-with-redux/widgets/src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-// import Accordion from "./components/Accordion";
-// import Dropdown from "./components/Dropdown";
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-// import Search from "./components/Search"
+import Search from "./components/Search";
+import Route from "./components/Route";
+
+const dropDownOptions = [
+  { label: "Black", value: "#0000" },
+  { label: "White", value: "#FFFF" },
+];
 
 const App = () => {
+  const [selected, setSelected] = useState(dropDownOptions[0]);
   return (
     <>
-      <Translate />
-      {/* <Accordion /> */}
+      <Route path="/">
+        <Accordion />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown label="select color" selected={selected} options={dropDownOptions} onSelectedChange={setSelected}  />
+      </Route>
     </>
   );
 };

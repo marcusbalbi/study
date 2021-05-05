6e640832bb0ec0aa413ff6a6d59238a1848978e4
Marcus Balbi <balbimarcus@gmail.com>
2021-05-05 18:19:43 -0300

    currentpath working inside Route component

--- udemy/modern-react-with-redux/widgets/src/components/Route.js
+++ udemy/modern-react-with-redux/widgets/src/components/Route.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   useEffect(() => {
-    const onLocationChange = (a) => { console.log(a) };
+    const onLocationChange = (ev) => {
+      setCurrentPath(ev.target.location.pathname);
+    };
     window.addEventListener("popstate", onLocationChange);
 
     return () => {
       window.removeEventListener("popstate", onLocationChange);
     };
   }, []);
-  return window.location.pathname === path ? children : null;
+  return currentPath === path ? children : null;
 };
 
 export default Route;

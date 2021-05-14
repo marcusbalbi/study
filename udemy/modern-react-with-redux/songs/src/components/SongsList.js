5b9a0603d33a01ab14ad5ba6d8ceddceee310a2c
Marcus Balbi <balbimarcus@gmail.com>
2021-05-14 18:12:06 -0300

    songs list component

--- udemy/modern-react-with-redux/songs/src/components/SongsList.js
+++ udemy/modern-react-with-redux/songs/src/components/SongsList.js
@@ -2,17 +2,24 @@ import React from "react";
 import { useSelector } from "react-redux";
 const SongsList = () => {
   const songs = useSelector((state) => state.songs);
-  console.log(songs);
+
+  function renderList() {
+    return songs.map((song) => {
+      return (
+        <div className="item" key={songs.title}>
+          <div className="right floated content">
+            <button className="ui primary button"> Select </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   return (
-    <ul>
-      {songs.map((song) => {
-        return (
-          <li>
-            {song.title} - {song.duration}
-          </li>
-        );
-      })}
-    </ul>
+    <div className="ui divided list" >
+      {renderList()}
+    </div>
   );
 };
 

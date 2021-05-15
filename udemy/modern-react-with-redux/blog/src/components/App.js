49ffc1a85f7f10cca7db08d73af5ad626814af94
Marcus Balbi <balbimarcus@gmail.com>
2021-05-15 16:03:31 -0300

    add dependencies

---
+++ udemy/modern-react-with-redux/blog/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="ui container" >
+      <h1>OLA MUNDO</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ udemy/modern-react-with-redux/blog/src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  replaceMe: () => 1,
+});
+
+export default reducers;

8032b741f9c563912a11cc2137ebd0da3ecbb5bf
Marcus Balbi <balbimarcus@gmail.com>
2021-05-24 20:11:12 -0300

    add react-router

--- udemy/modern-react-with-redux/streams/client/src/components/App.js
+++ udemy/modern-react-with-redux/streams/client/src/components/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, useHistory } from "react-router-dom";
+
+const PageOne = () => {
+  const history = useHistory();
+  return (
+    <div>
+      PAGE 1{" "}
+      <button onClick={() => history.push("/page2")}>Go to Page 2</button>{" "}
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  const history = useHistory();
+  return (
+    <div>
+      PAGE 2 <button onClick={() => history.push("/")}>Go to Page 1</button>{" "}
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>OLA</div>;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={PageOne} />
+      <Route exact path="/page2" component={PageTwo} />
+    </BrowserRouter>
+  );
 };
 
 export default App;

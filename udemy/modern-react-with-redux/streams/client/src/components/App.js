a6fc67f85ac2052b4685989445110fa2ddfb39ec
Marcus Balbi <balbimarcus@gmail.com>
2021-05-24 20:31:58 -0300

    navigate with Link

--- udemy/modern-react-with-redux/streams/client/src/components/App.js
+++ udemy/modern-react-with-redux/streams/client/src/components/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route, useHistory } from "react-router-dom";
+import { BrowserRouter, Link, Route, useHistory, useParams } from "react-router-dom";
 
 const PageOne = () => {
   const history = useHistory();
   return (
     <div>
-      PAGE 1{" "}
-      <button onClick={() => history.push("/page2")}>Go to Page 2</button>{" "}
+      PAGE 1
+      <button onClick={() => history.push("/page2/3")}>Go to Page 2</button>
+      <br />
+      <Link to="/page2/3">Go to Page 2</Link>
     </div>
   );
 };
 
 const PageTwo = () => {
   const history = useHistory();
+  let { id } = useParams();
+  console.log(id)
   return (
     <div>
-      PAGE 2 <button onClick={() => history.push("/")}>Go to Page 1</button>{" "}
+      PAGE 2 <button onClick={() => history.push("/")}>Go to Page 1</button>
+      <br />
+      <Link to="/">Go to Page 1</Link>
     </div>
   );
 };
@@ -24,7 +30,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={PageOne} />
-      <Route exact path="/page2" component={PageTwo} />
+      <Route exact path="/page2/:id" component={PageTwo} />
     </BrowserRouter>
   );
 };

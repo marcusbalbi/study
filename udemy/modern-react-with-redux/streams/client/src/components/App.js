bf39d3440f772a96cfb5935219bd076d6a4e4f45
Marcus Balbi <balbimarcus@gmail.com>
2021-05-25 20:02:39 -0300

    Header component

--- udemy/modern-react-with-redux/streams/client/src/components/App.js
+++ udemy/modern-react-with-redux/streams/client/src/components/App.js
@@ -5,16 +5,20 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={StreamList} />
-      <Route exact path="/streams/new" component={StreamCreate} />
-      <Route exact path="/streams/delete" component={StreamDelete} />
-      <Route exact path="/streams/edit" component={StreamEdit} />
-      <Route exact path="/streams/show" component={StreamShow} />
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={StreamList} />
+        <Route exact path="/streams/new" component={StreamCreate} />
+        <Route exact path="/streams/delete" component={StreamDelete} />
+        <Route exact path="/streams/edit" component={StreamEdit} />
+        <Route exact path="/streams/show" component={StreamShow} />
+      </BrowserRouter>
+    </div>
   );
 };
 

8499a3ff9159dd9ac0f5a94fdf76d226e1a06e9d
Marcus Balbi <balbimarcus@gmail.com>
2021-06-02 21:24:01 -0300

    install oauth google

---
+++ udemy/modern-react-with-redux/streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  let auth = useRef();
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_OAUTH_CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          auth.current = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.current.isSignedIn.get());
+        });
+    });
+  }, []);
+
+  function renderAuthButton() {
+    if (isSignedIn === null) {
+      return <div>"loading..."</div>;
+    } else if (isSignedIn === true) {
+      return <div>"SignedIN!"</div>;
+    } else {
+      return <div>"Not SIGNEDIN!"</div>;
+    }
+  }
+
+  return <div>{renderAuthButton()}</div>;
+};
+
+export default GoogleAuth;

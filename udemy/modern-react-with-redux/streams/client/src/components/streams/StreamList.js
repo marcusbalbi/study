125715593a07c7ca9a329a868d19be4b4b5d36f8
Marcus Balbi <balbimarcus@gmail.com>
2021-06-30 17:59:32 -0300

    render stream list

--- udemy/modern-react-with-redux/streams/client/src/components/streams/StreamList.js
+++ udemy/modern-react-with-redux/streams/client/src/components/streams/StreamList.js
@@ -1,13 +1,34 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchStreams } from "../../store/actions";
 
 const StreamList = () => {
   const dispath = useDispatch();
+  const streams = Object.values(useSelector((state) => state.streams));
+
+  const renderList = () => {
+    return streams.map((item) => {
+      return (
+        <div className="item" key={item.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {item.title}
+            <div className="description">{item.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   useEffect(() => {
     dispath(fetchStreams());
   }, [dispath]);
-  return <h2>List Streams</h2>;
+  return (
+    <div>
+      <h2>Streams:</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
 export default StreamList;

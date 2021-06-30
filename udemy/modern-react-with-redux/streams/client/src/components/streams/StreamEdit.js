ce28fa7a75dd82a434ef91086147ba120bd9f6ca
Marcus Balbi <balbimarcus@gmail.com>
2021-06-30 20:29:12 -0300

    Edit loading from server

--- udemy/modern-react-with-redux/streams/client/src/components/streams/StreamEdit.js
+++ udemy/modern-react-with-redux/streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStream } from "../../store/actions";
 
-const StreamEdit = (props) => {
-  console.log(props.match.params.id);
-  const stream = useSelector((state) => state.streams[props.match.params.id]);
+const StreamEdit = ({ match }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchStream(match.params.id));
+  }, [dispatch, match.params.id]);
+  const stream = useSelector((state) => state.streams[match.params.id]);
+  if (!stream) {
+    return <div>Loading...</div>
+  }
   return <h2>Edit a Stream: {JSON.stringify(stream)}</h2>;
 };
 

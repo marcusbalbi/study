8d6dcd1af3be0e3996aadeee8e82ddef4be4c800
Marcus Balbi <balbimarcus@gmail.com>
2021-06-30 17:13:31 -0300

    create actions

--- udemy/modern-react-with-redux/streams/client/src/store/actions/index.js
+++ udemy/modern-react-with-redux/streams/client/src/store/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "../types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+} from "../types";
 
 export const signIn = (userId) => {
   return {
@@ -14,9 +22,57 @@ export const signOut = () => {
   };
 };
 
-
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post('/streams/', formValues)
-  }
+    const response = await streams.post("/streams/", formValues);
+
+    dispatch({
+      type: CREATE_STREAM,
+      payload: response.data,
+    });
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get("/streams/");
+
+    dispatch({
+      type: FETCH_STREAMS,
+      payload: response.data,
+    });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({
+      type: FETCH_STREAM,
+      payload: response.data,
+    });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({
+      type: DELETE_STREAM,
+      payload: id,
+    });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({
+      type: EDIT_STREAM,
+      payload: response.data,
+    });
+  };
 };

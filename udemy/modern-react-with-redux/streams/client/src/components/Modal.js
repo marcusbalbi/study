c05840e2b62753f3a74cbbc927a41add4aad5a49
Marcus Balbi <balbimarcus@gmail.com>
2021-07-01 21:06:12 -0300

    improve Modal

--- udemy/modern-react-with-redux/streams/client/src/components/Modal.js
+++ udemy/modern-react-with-redux/streams/client/src/components/Modal.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useHistory } from "react-router-dom";
 
 const Modal = (props) => {
+  const history = useHistory();
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        lorem ipsum dolor sit amet, consectetur adip
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Remover Stream</div>
+        <div className="content">
+          Tem certeza que deseja excluir esta Stream ?
+        </div>
+        <div className="actions">
+          <button className="ui button primary">Remover</button>
+          <button className="ui button">Cancelar</button>
+        </div>
       </div>
     </div>,
     document.querySelector("#modal")

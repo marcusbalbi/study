b79095a3f8a0d42f0f78103236beb4b1896b92e3
Marcus Balbi <balbimarcus@gmail.com>
2021-07-03 14:21:52 -0300

    add language state and html

--- udemy/modern-react-with-redux/translate/src/components/App.js
+++ udemy/modern-react-with-redux/translate/src/components/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const App = () => {
+  const [language, setLanguage] = useState("english");
+
   return (
-    <div className="ui container" >App</div>
-  )
-}
+    <div className="ui container">
+      <div>
+        Select a Language:
+        <i className="flag us" onClick={() => setLanguage("english")} />
+        <i className="flag fr" onClick={() => setLanguage("french")} />
+      </div>
+    </div>
+  );
+};
 
 export default App;

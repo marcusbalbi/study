b57b3e568a1963b463fa53c0c664507a3b9992e3
Marcus Balbi <balbimarcus@gmail.com>
2021-07-03 15:07:59 -0300

    two contexts

--- udemy/modern-react-with-redux/translate/src/components/Button.js
+++ udemy/modern-react-with-redux/translate/src/components/Button.js
@@ -1,9 +1,18 @@
 import React, { useContext } from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ThemeContext from "../contexts/ThemeContext";
 const Button = () => {
   const language = useContext(LanguageContext);
+  const theme = useContext(ThemeContext);
   const text = language === "french" ? "Envoyer" : "Send";
-  return <button className="ui button primary">{text}</button>;
+  return (
+    <button
+      className="ui button"
+      style={{ backgroundColor: theme.primaryColor }}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;

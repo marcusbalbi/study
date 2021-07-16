0bd714ce709c59c3a78bc1a06f8008cb582045d1
Marcus Balbi <balbimarcus@gmail.com>
2021-07-15 21:06:46 -0300

    caches all files important

--- udemy/pwa-the-complete-guide/main-project/public/sw.js
+++ udemy/pwa-the-complete-guide/main-project/public/sw.js
@@ -3,7 +3,19 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("static").then((cache) => {
       console.log("[Service Worker] Precaching App shell");
-      cache.addAll(["/", "/index.html", "src/js/app.js"]);
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/iamges/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });

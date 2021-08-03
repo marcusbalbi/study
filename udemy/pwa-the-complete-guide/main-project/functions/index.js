8a7e42e221acefb3788b7cd116237a7af2a2494f
Marcus Balbi <balbimarcus@gmail.com>
2021-08-02 21:59:12 -0300

    config backend push

--- udemy/pwa-the-complete-guide/main-project/functions/index.js
+++ udemy/pwa-the-complete-guide/main-project/functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({origin: true});
+const webPush = require("web-push");
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -26,6 +28,28 @@ exports.storePostData = functions.https.onRequest((request, response) => {
       location: request.body.location,
       image: request.body.image,
     }).then(() => {
+      webPush.setVapidDetails(
+          "mailto:balbimarcus@gmail.com",
+          // eslint-disable-next-line
+          "BA5Kjx6HISaokoS5YRKLfWpi-gwE4Ly9L98uK_aMPxGXWsoVnmbVRKS83prFhzEzxoDxSmJTi4UUTFMAXCOMmf4",
+          "vc2H0PFG8DXqdJnUiH8AwU5V7aXaBlArFvkZtyGSSu4"
+      );
+      return admin.database().ref("subsscriptions").once("value");
+    }).then((subs) => {
+      subs.forEach((sub) => {
+        const pushConfig = {
+          endpoint: sub.val().endpoint,
+          keys: {
+            auth: sub.val().keys.auth,
+            p256dh: sub.val().keys.p256dh,
+          },
+        };
+        webPush.sendNotification(pushConfig,
+            JSON.stringify({title: "New Post!", content: "New post Added!"}))
+            .catch((err) => {
+              console.log(err);
+            });
+      });
       response.status(201)
           .json({message: "data Stored", id: request.body.id});
     }).catch((err) => {

8fb3dbbf4a68981cc50805b1cf063a3ba90a369c
Marcus Balbi <balbimarcus@gmail.com>
2021-08-02 21:06:54 -0300

    notification on sw

--- udemy/pwa-the-complete-guide/main-project/public/sw.js
+++ udemy/pwa-the-complete-guide/main-project/public/sw.js
@@ -210,3 +210,15 @@ self.addEventListener("sync", function (event) {
     );
   }
 });
+
+self.addEventListener('notificationclick', function(event) {
+  const notification = event.notification;
+  const action = event.action;
+
+  console.log(notification);
+
+  if (action === "confirm") {
+    console.log("confirmed!")
+    notification.close();
+  }
+})

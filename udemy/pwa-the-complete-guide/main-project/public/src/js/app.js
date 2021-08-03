5efe96a579dfbd7125e8faf3d31259d2eea875ce
Marcus Balbi <balbimarcus@gmail.com>
2021-08-02 21:42:18 -0300

    subscrition

--- udemy/pwa-the-complete-guide/main-project/public/src/js/app.js
+++ udemy/pwa-the-complete-guide/main-project/public/src/js/app.js
@@ -18,7 +18,7 @@ window.addEventListener("beforeinstallprompt", (e) => {
   console.log(`'beforeinstallprompt' event was fired.`);
 });
 
-function displayConfirmPermission() {
+function displayConfirmNotification() {
   const options = {
     body: "Obrigado por se registrar no app!",
     icon: "/src/images/icons/app-icon-96x96.png",
@@ -50,15 +50,60 @@ function displayConfirmPermission() {
   }
 }
 
+function configPubSub() {
+  if (!("serviceWorker" in navigator)) {
+    return;
+  }
+  let reg = null;
+  navigator.serviceWorker.ready
+    .then((swreg) => {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then((sub) => {
+      if (null === sub) {
+        let vapidPublicKey =
+          "BA5Kjx6HISaokoS5YRKLfWpi-gwE4Ly9L98uK_aMPxGXWsoVnmbVRKS83prFhzEzxoDxSmJTi4UUTFMAXCOMmf4";
+        const converted = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: converted,
+        });
+      } else {
+      }
+    })
+    .then((newSub) => {
+      return fetch(
+        "https://balbigram-default-rtdb.firebaseio.com/subscriptions.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(newSub),
+        }
+      );
+    })
+    .then((res) => {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission().then((result) => {
     if (result === "granted") {
-      displayConfirmPermission();
+      configPubSub();
     }
   });
 }
 
-if ("Notification" in window) {
+if ("Notification" in window && "serviceWorker" in navigator) {
   enablePushNotificationsButtons.forEach((button) => {
     button.style.display = "inline-block";
     button.addEventListener("click", askForNotificationPermission);

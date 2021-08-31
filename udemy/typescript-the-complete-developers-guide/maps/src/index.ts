420b73eb4071fee51e41c70e185aa5e17ffb6f41
Marcus Balbi <balbimarcus@gmail.com>
2021-08-31 17:24:53 -0300

    maps included

--- udemy/typescript-the-complete-developers-guide/maps/src/index.ts
+++ udemy/typescript-the-complete-developers-guide/maps/src/index.ts
@@ -1,5 +1,8 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
 
-console.log(user);
+console.log(user, company);

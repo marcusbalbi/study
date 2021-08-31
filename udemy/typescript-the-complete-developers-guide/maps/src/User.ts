fb3827314975399c68014c85f44411a8d7dbcd76
Marcus Balbi <balbimarcus@gmail.com>
2021-08-31 16:53:14 -0300

    create user

--- udemy/typescript-the-complete-developers-guide/maps/src/User.ts
+++ udemy/typescript-the-complete-developers-guide/maps/src/User.ts
@@ -1,11 +1,17 @@
 import faker from "faker";
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }

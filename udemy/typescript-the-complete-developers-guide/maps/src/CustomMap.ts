d7360c19adb321e2d5f6a72173fccdc87fcd75f6
Marcus Balbi <balbimarcus@gmail.com>
2021-08-31 18:31:44 -0300

    bad example

--- udemy/typescript-the-complete-developers-guide/maps/src/CustomMap.ts
+++ udemy/typescript-the-complete-developers-guide/maps/src/CustomMap.ts
@@ -1,4 +1,8 @@
 /// <reference types="@types/google.maps" />
+
+import { Company } from "./Company";
+import { User } from "./User";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -8,4 +12,9 @@ export class CustomMap {
       center: { lat: 0, lng: 0 },
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({ map: this.googleMap, position: user.location });
+  }
+  addCompanyMarker(company: Company): void {}
 }
--- udemy/typescript-the-complete-developers-guide/maps/src/index.ts
+++ udemy/typescript-the-complete-developers-guide/maps/src/index.ts
@@ -5,6 +5,7 @@ import { CustomMap } from "./CustomMap";
 const user = new User();
 const company = new Company();
 const map = new CustomMap("map");
+
+map.addUserMarker(user);
+
 console.log(user, company);
-
-

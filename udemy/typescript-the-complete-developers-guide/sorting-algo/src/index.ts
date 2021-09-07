ed21aafe3b0e597636be6afd76e19f6840c6bfc6
Marcus Balbi <balbimarcus@gmail.com>
2021-09-07 15:55:30 -0300

    sorting algo initi

---
+++ udemy/typescript-the-complete-developers-guide/sorting-algo/src/index.ts
@@ -0,0 +1,21 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([15, 12, -10, 0, -5, 23]);
+sorter.sort();
+console.log(sorter.collection);

b8bfc5bb9bab39fa6969c9b2e02a7cf4b9572e24
Marcus Balbi <balbimarcus@gmail.com>
2021-09-14 14:11:23 -0300

    convert date

--- udemy/typescript-the-complete-developers-guide/stats/src/CsvFileReader.ts
+++ udemy/typescript-the-complete-developers-guide/stats/src/CsvFileReader.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { dateStringToDate } from "./utils";
 
 export class CsvFileReader {
   public data: string[][] = [];
@@ -11,6 +12,15 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
+      })
+      .map((row: string[]) => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+        ];
       });
   }
 }
---
+++ udemy/typescript-the-complete-developers-guide/stats/src/utils.ts
@@ -0,0 +1,5 @@
+export const dateStringToDate = (dateString: string): Date => {
+  const dateParts = dateString.split("/").map((part) => parseInt(part));
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};

f1b9144d1c0c13c6a5912d8da54647cb12e9602a
Marcus Balbi <balbimarcus@gmail.com>
2021-09-17 10:49:58 -0300

    class user set

--- udemy/typescript-the-complete-developers-guide/web/src/index.ts
+++ udemy/typescript-the-complete-developers-guide/web/src/index.ts
@@ -3,3 +3,6 @@ import { User } from "./models/User";
 const user = new User({ name: "Balbi", age: 32 });
 
 console.log(user.get("name"));
+user.set({ age: 27, name: "Marcus Balbi" });
+console.log(user.get("age"));
+console.log(user.get("name"));
--- udemy/typescript-the-complete-developers-guide/web/src/models/User.ts
+++ udemy/typescript-the-complete-developers-guide/web/src/models/User.ts
@@ -1,11 +1,15 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 export class User {
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
 }

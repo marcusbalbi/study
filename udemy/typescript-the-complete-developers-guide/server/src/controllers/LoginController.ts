9292962b20bc68e9b63b67d5b2b0399a08c54888
Marcus Balbi <balbimarcus@gmail.com>
2021-09-25 10:19:33 -0300

    first decorator get

---
+++ udemy/typescript-the-complete-developers-guide/server/src/controllers/LoginController.ts
@@ -0,0 +1,22 @@
+import { Response } from "express";
+import { get } from "./decorators/routes";
+
+@controller("/")
+export class LoginController {
+  @get("/login")
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit!</button>
+    </form>
+  `);
+  }
+}
--- udemy/typescript-the-complete-developers-guide/server/src/routes/login.routes.ts
+++ udemy/typescript-the-complete-developers-guide/server/src/routes/login.routes.ts
@@ -14,21 +14,7 @@ const requireAuth = (req: Request, res: Response, next: NextFunction) => {
   return res.status(403).send("Not Allowed!");
 };
 
-router.get("/login", (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-      <div>
-        <label>Email</label>
-        <input name="email" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input name="password" type="password" />
-      </div>
-      <button>Submit!</button>
-    </form>
-  `);
-});
+
 
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;

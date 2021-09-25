6f2423fc0c98ba0b4434c6e0b5f04d3f40253487
Marcus Balbi <balbimarcus@gmail.com>
2021-09-24 21:16:59 -0300

    request interface

--- udemy/typescript-the-complete-developers-guide/server/src/routes/login.routes.ts
+++ udemy/typescript-the-complete-developers-guide/server/src/routes/login.routes.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,10 +22,14 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  res.send(`${email}:${password}`);
+  if (email) {
+    res.send(`${email.toUpperCase()}:${password}`);
+  } else {
+    res.status(422).send("Error: Email is required!");
+  }
 });
 
 export { router };

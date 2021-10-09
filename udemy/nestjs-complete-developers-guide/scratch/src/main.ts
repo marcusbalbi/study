3eff0ed8b053f67bd2a606fb5a9e2be79dc892b2
Marcus Balbi <balbimarcus@gmail.com>
2021-10-09 19:09:12 -0300

    working

---
+++ udemy/nestjs-complete-developers-guide/scratch/src/main.ts
@@ -0,0 +1,23 @@
+import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return "hi there!";
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+
+bootstrap();  

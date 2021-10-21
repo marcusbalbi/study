0c75e9834a75642c35b36d0197ce77095413b9da
Marcus Balbi <balbimarcus@gmail.com>
2021-10-21 18:13:18 -0300

    integrate reports module

--- udemy/nestjs-complete-developers-guide/mycarvalue/src/reports/reports.module.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}

d265915c1138de3313ac0858ba7f6d7fe63b4f1c
Marcus Balbi <balbimarcus@gmail.com>
2021-10-21 17:56:48 -0300

    integrate typeorm and nest

--- udemy/nestjs-complete-developers-guide/mycarvalue/src/app.module.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

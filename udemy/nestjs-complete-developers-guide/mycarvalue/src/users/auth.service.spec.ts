b3ae026c9cd1930f8123e0897eac27dbf586b187
Marcus Balbi <balbimarcus@gmail.com>
2021-10-27 08:22:02 -0300

    fake user service

--- udemy/nestjs-complete-developers-guide/mycarvalue/src/users/auth.service.spec.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/users/auth.service.spec.ts
@@ -1,13 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 it('can create a instance of auth service', async () => {
   // Fake user service
-  const fakeUserService = {
+  const fakeUserService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+    create: (newuser: CreateUserDto) =>
+      Promise.resolve({
+        id: 1,
+        email: newuser.email,
+        password: newuser.password,
+      } as User),
   };
   const module = await Test.createTestingModule({
     providers: [

ff3d3198bb03af4c39f22e7ba22f1b38b8f6b7b1
Marcus Balbi <balbimarcus@gmail.com>
2021-10-29 07:47:39 -0300

    test whoami

--- udemy/nestjs-complete-developers-guide/mycarvalue/test/auth.e2e-spec.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/test/auth.e2e-spec.ts
@@ -27,4 +27,21 @@ describe('Auth (e2e)', () => {
         expect(emailCreated).toBe(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'abcde3323@test.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'test123' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });

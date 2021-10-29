e19d7960d32a2030d463fd116cba408d41a9ceac
Marcus Balbi <balbimarcus@gmail.com>
2021-10-29 07:54:52 -0300

    start report module

---
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,15 @@
+export class CreateReportDto {
+  price: number;
+
+  make: string;
+
+  model: string;
+
+  year: number;
+
+  lng: number;
+
+  lat: number;
+
+  mileage: number;
+}
--- udemy/nestjs-complete-developers-guide/mycarvalue/src/reports/reports.controller.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/reports/reports.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+  @Post()
+  createReport(@Body() body: CreateReportDto) {
+    
+  }
+}

a33dfdf966662a48e0e4cf6dd0f9a36ef00302df
Marcus Balbi <balbimarcus@gmail.com>
2021-10-30 11:54:16 -0300

    current user middleware

---
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../user.entity';
+import { UsersService } from '../users.service';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private service: UsersService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session || {};
+
+    if (userId) {
+      const user = await this.service.findOne(userId);
+      req.currentUser = user;
+    }
+
+    next();
+  }
+}
--- udemy/nestjs-complete-developers-guide/mycarvalue/src/users/users.module.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -13,10 +14,14 @@ import { UsersService } from './users.service';
   providers: [
     UsersService,
     AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor,
+    // },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}

d8ca4bdd61987035a91491a3e9106de7b7f0c0e3
Marcus Balbi <balbimarcus@gmail.com>
2021-11-02 10:18:11 -0300

    changes do deploy

--- udemy/nestjs-complete-developers-guide/mycarvalue/ormconfig.js
+++ udemy/nestjs-complete-developers-guide/mycarvalue/ormconfig.js
@@ -23,6 +23,15 @@ switch (process.env.NODE_ENV) {
     break;
   }
   case 'production': {
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
   }
   default: {

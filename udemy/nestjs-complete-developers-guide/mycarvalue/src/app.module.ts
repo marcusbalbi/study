e73295237edc86e534f05df143068c6e92151c9e
Marcus Balbi <balbimarcus@gmail.com>
2021-11-02 08:08:47 -0300

    cookie_key

--- udemy/nestjs-complete-developers-guide/mycarvalue/src/app.module.ts
+++ udemy/nestjs-complete-developers-guide/mycarvalue/src/app.module.ts
@@ -49,11 +49,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdas'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');

75e240e6ed68f7064c9ab4cfa7af71b8cd772f2a
Marcus Balbi <balbimarcus@gmail.com>
2021-11-02 10:02:57 -0300

    migration created

--- udemy/nestjs-complete-developers-guide/mycarvalue/ormconfig.js
+++ udemy/nestjs-complete-developers-guide/mycarvalue/ormconfig.js
@@ -1,4 +1,8 @@
-const dbConfig = { synchronize: false };
+const dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: { migrationsDir: 'migrations' },
+};
 
 switch (process.env.NODE_ENV) {
   case 'development': {

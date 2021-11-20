e717ffd6b3d3a748f57c5b0c2b56d2ec88ceb4cb
Marcus Balbi <balbimarcus@gmail.com>
2021-11-20 12:17:08 -0300

    moment timezone config

--- udemy/nodejs-microservices-nestjs-rabbitmq/api-smartranking/src/main.ts
+++ udemy/nodejs-microservices-nestjs-rabbitmq/api-smartranking/src/main.ts
@@ -3,11 +3,18 @@ require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   await app.listen(8080);
 }

f3d6006bc284ea6a674343493ea578cb47d8f2e6
Marcus Balbi <balbimarcus@gmail.com>
2021-11-20 10:34:04 -0300

    using global filter

---
+++ udemy/nodejs-microservices-nestjs-rabbitmq/api-smartranking/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private logger: Logger;
+  constructor() {
+    this.logger = new Logger(AllExceptionsFilter.name);
+  }
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse();
+    const request: Request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse : '';
+
+    this.logger.error(
+      `Http status: ${status} Error Message: ${JSON.stringify(message)}`,
+    );
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
--- udemy/nodejs-microservices-nestjs-rabbitmq/api-smartranking/src/main.ts
+++ udemy/nodejs-microservices-nestjs-rabbitmq/api-smartranking/src/main.ts
@@ -2,9 +2,13 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(8080);
 }
 bootstrap();

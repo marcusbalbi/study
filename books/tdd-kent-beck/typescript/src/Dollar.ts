db7e82c77cdba33dcda7b48472002f7ac93ff4d3
Marcus Balbi <balbimarcus@gmail.com>
2021-12-20 18:48:34 -0300

    fix side effect variable change

--- books/tdd-kent-beck/typescript/src/Dollar.ts
+++ books/tdd-kent-beck/typescript/src/Dollar.ts
@@ -1,6 +1,6 @@
 export class Dollar {
   constructor(public amount: number) {}
-  times(multiplier: number): void {
-    this.amount = this.amount * multiplier
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier)
   }
 }
--- books/tdd-kent-beck/typescript/tests/dollar.spec.ts
+++ books/tdd-kent-beck/typescript/tests/dollar.spec.ts
@@ -1,7 +1,11 @@
 import { Dollar } from '../src/Dollar'
 
-test('should return 10 when five dollars mutiply by 2', () => {
-  const five = new Dollar(5)
-  five.times(2)
-  expect(five.amount).toEqual(10)
+describe('Dollar', () => {
+  test('should return 10 when five dollars mutiply by 2', () => {
+    const five = new Dollar(5)
+    let product: Dollar = five.times(2)
+    expect(product.amount).toEqual(10)
+    product = five.times(3)
+    expect(product.amount).toEqual(15)
+  })
 })

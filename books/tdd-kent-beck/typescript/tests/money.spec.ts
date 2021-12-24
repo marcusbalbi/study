5ba468bf411e0749553869ebc8070f08ecb1a047
Marcus Balbi <balbimarcus@gmail.com>
2021-12-24 14:45:46 -0300

    remove useless tets

--- books/tdd-kent-beck/typescript/tests/money.spec.ts
+++ books/tdd-kent-beck/typescript/tests/money.spec.ts
@@ -1,29 +1,15 @@
 import { Money } from '../src/Money'
 
 describe('Money', () => {
-  test('should handle dollar multiplication', () => {
+  test('should handle money multiplication', () => {
     const five: Money = Money.dollar(5)
     expect(five.times(2).equals(Money.dollar(10))).toBe(true)
     expect(five.times(3).equals(Money.dollar(15))).toBe(true)
   })
 
-  test('should handle Euro multiplication', () => {
-    const five = Money.euro(5)
-    expect(five.times(2).equals(Money.euro(10))).toBe(true)
-    expect(five.times(3).equals(Money.euro(15))).toBe(true)
-  })
-
-  test('should compare dollars with dollars', () => {
+  test('should compare money correclty', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true)
     expect(Money.dollar(5).equals(Money.dollar(10))).toBe(false)
-  })
-
-  test('should compare euros with euros', () => {
-    expect(Money.euro(5).equals(Money.euro(5))).toBe(true)
-    expect(Money.euro(5).equals(Money.euro(10))).toBe(false)
-  })
-
-  test('should compare dollars with euros', () => {
     expect(Money.dollar(5).equals(Money.euro(5))).toBe(false)
   })
 

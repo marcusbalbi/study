09fef2243866dc72861c2928c34e36e5cf1edb2e
Marcus Balbi <balbimarcus@gmail.com>
2021-12-29 18:30:48 -0300

    start use case

---
+++ books/arquitetura-limpa-na-pratica/src/usecases/register-user-on-mailling-list/register-user-on-mailling-list.spec.ts
@@ -0,0 +1,16 @@
+import { UserData } from './user-data';
+
+describe('Register user on mailling list use case', () => {
+  test('should add user with complete data to mailling list', async () => {
+    const users: UserData[] = [];
+    console.log(users);
+    // const repo: UserRepository = new InMemoryUserRepository(users);
+    // const usecase = new RegisterUserOnMaillingList(repo);
+    // const name = 'any_name';
+    // const email = 'any@email.com';
+
+    // const response = await usecase.registerUserOnMaillingList({ name, email });
+    // const user = await repo.findUserByEmail(email);
+    // expect(user.name).toBe(name);
+  });
+});

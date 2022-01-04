0ec265fc5f416409dae1e18254f08c44080102f2
Marcus Balbi <balbimarcus@gmail.com>
2022-01-04 20:12:31 -0300

    jest mongodb

--- books/arquitetura-limpa-na-pratica/jest.config.js
+++ books/arquitetura-limpa-na-pratica/jest.config.js
@@ -100,7 +100,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: '@shelf/jest-mongodb',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -142,7 +142,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},

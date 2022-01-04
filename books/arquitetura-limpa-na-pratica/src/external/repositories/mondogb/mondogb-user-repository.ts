4cb1888130d76cdbf4564924c9bd495b463d43ea
Marcus Balbi <balbimarcus@gmail.com>
2022-01-04 20:56:18 -0300

    start implementing mongo

--- books/arquitetura-limpa-na-pratica/src/external/repositories/mondogb/mondogb-user-repository.ts
+++ books/arquitetura-limpa-na-pratica/src/external/repositories/mondogb/mondogb-user-repository.ts
@@ -1,20 +1,53 @@
 import { UserData } from '@/entities';
 import { UserRepository } from '@/usecases/register-user-on-mailling-list/ports';
+import { MongoHelper } from './helper';
 
 export class MongodbUserRepository implements UserRepository {
-  add(user: UserData): Promise<void> {
-    throw new Error('Method not implemented.');
+  async add(user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users');
+    const exists = await this.exists(user);
+    if (!exists) {
+      await userCollection.insertOne(user);
+    }
   }
 
-  findUserByEmail(email: string): Promise<UserData> {
-    throw new Error('Method not implemented.');
+  async findUserByEmail(email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users');
+    const user = await userCollection.findOne({ email });
+
+    if (!user) {
+      return null;
+    }
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 
-  findAllUsers(): Promise<UserData[]> {
-    throw new Error('Method not implemented.');
+  async findAllUsers(): Promise<UserData[]> {
+    return [];
+    // const userCollection = MongoHelper.getCollection('users');
+
+    // const users = await userCollection.find();
+
+    // if (!users) {
+    //   return [];
+    // }
+    // return users.map((u) => {
+    //   return {
+    //     name: u.name,
+    //     email: u.email,
+    //   };
+    // });
   }
 
-  exists(user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  async exists(user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email);
+
+    if (result != null) {
+      return true;
+    }
+    return false;
   }
 }

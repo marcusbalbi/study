4bc19b3b11be2a7cbd7429b9f8d2a3e0988adbae
Marcus Balbi <balbimarcus@gmail.com>
2022-01-04 21:35:01 -0300

    example attachemnt

---
+++ books/arquitetura-limpa-na-pratica/tests/usecases/send-email/send-email.spec.ts
@@ -0,0 +1,17 @@
+describe('Send Email', () => {
+  const attachmentFilePath = '../resources/text.txt'
+  const fromName = 'Test';
+  const fromEmail = 'from_email@mail.com';
+  const toName = 'To Test';
+  const toEmail = 'to_email@mail.com';
+  const subject = 'test e-mail';
+  const body = 'Hello world!';
+  const bodyHtml = '<b>Test</b>';
+  const attachment = [
+    {
+      filename: attachmentFilePath,
+      contentType: 'text/plain',
+    },
+  ];
+  test('');
+});

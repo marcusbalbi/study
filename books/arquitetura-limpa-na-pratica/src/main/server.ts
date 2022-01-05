e571ff6f17b4f124d50c80acca3796e50845aee3
Marcus Balbi <balbimarcus@gmail.com>
2022-01-04 21:24:14 -0300

    connect to local mongodb

--- books/arquitetura-limpa-na-pratica/src/main/server.ts
+++ books/arquitetura-limpa-na-pratica/src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register';
 import { MongoHelper } from '@/external/repositories/mondogb/helper';
 
-MongoHelper.connect(process.env.MONGO_URL)
+MongoHelper.connect('mongodb://localhost:27017')
   .then(async () => {
     const app = (await import('./config/app')).default;
     app.listen(5000, () => {

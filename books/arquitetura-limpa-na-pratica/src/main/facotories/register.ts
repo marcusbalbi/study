557e2ddf17976b7ea7184cf3f132eced0a91788d
Marcus Balbi <balbimarcus@gmail.com>
2022-01-07 19:51:42 -0300

    fix test problem

--- books/arquitetura-limpa-na-pratica/src/main/facotories/register.ts
+++ books/arquitetura-limpa-na-pratica/src/main/facotories/register.ts
@@ -1,10 +1,8 @@
 import { MongodbUserRepository } from '@/external/repositories/mondogb/mondogb-user-repository';
-import { RegisterUserAndSendEmailUseCase } from '@/usecases/register-user-and-send-email/regiser-user-and-send-email';
+// import { RegisterUserAndSendEmailUseCase } from '@/usecases/register-user-and-send-email/regiser-user-and-send-email';
 import { RegisterUserOnMaillingList } from '@/usecases/register-user-on-mailling-list';
-import { SendEmail } from '@/usecases/send-email/send-email';
+// import { SendEmail } from '@/usecases/send-email/send-email';
 import { RegisterUserAndSendEmailController } from '@/web-controllers/register-user-and-send-email-controller';
-import { createEmailOptions } from '@tests/fixtures/email';
-import { MailServiceMock } from '@tests/utils/mocks/mail-service-mock';
 
 export const makeRegisterUserController =
   (): RegisterUserAndSendEmailController => {
@@ -12,11 +10,13 @@ export const makeRegisterUserController =
     const registerUserOnMaillingListUseCase = new RegisterUserOnMaillingList(
       mondogbUserRepository
     );
-    const sendEmail = new SendEmail(createEmailOptions(), new MailServiceMock()); // TODO change to valid values
-    const useCase = new RegisterUserAndSendEmailUseCase(
-      registerUserOnMaillingListUseCase,
-      sendEmail
-    );
+    // const sendEmail = new SendEmail(createEmailOptions(), new MailServiceMock()); // TODO change to valid values
+    // const useCase = new RegisterUserAndSendEmailUseCase(
+    //   registerUserOnMaillingListUseCase,
+    //   sendEmail
+    // );
 
-    return new RegisterUserAndSendEmailController(useCase);
+    return new RegisterUserAndSendEmailController(
+      registerUserOnMaillingListUseCase
+    );
   };

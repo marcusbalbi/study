8dec85e19b1c86c9cbe1ac84334e0d044d775f5a
Marcus Balbi <balbimarcus@gmail.com>
2022-01-07 22:02:01 -0300

    port

--- books/arquitetura-limpa-na-pratica/src/main/server.ts
+++ books/arquitetura-limpa-na-pratica/src/main/server.ts
@@ -4,8 +4,9 @@ import { MongoHelper } from '@/external/repositories/mondogb/helper';
 MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default;
-    app.listen(5000, () => {
-      console.log('server listening on 5000');
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
     });
   })
   .catch(console.error);

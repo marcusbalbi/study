0d60c5cbbc525865100dfa71de136c094af8210e
Marcus Balbi <balbimarcus@gmail.com>
2022-01-07 21:51:46 -0300

    add MONGO_URL

--- books/arquitetura-limpa-na-pratica/src/main/server.ts
+++ books/arquitetura-limpa-na-pratica/src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register';
 import { MongoHelper } from '@/external/repositories/mondogb/helper';
 
-MongoHelper.connect('mongodb://localhost:27017')
+MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default;
     app.listen(5000, () => {

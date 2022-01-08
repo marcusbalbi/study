f7bbfb845a3aa844f67ac0d716eb56622680ba4b
Marcus Balbi <balbimarcus@gmail.com>
2022-01-07 21:53:45 -0300

    dot env config

--- books/arquitetura-limpa-na-pratica/jest.config.js
+++ books/arquitetura-limpa-na-pratica/jest.config.js
@@ -87,7 +87,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@tests/(.*)$': '<rootDir>/tests/$1'
+    '^@tests/(.*)$': '<rootDir>/tests/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
@@ -130,7 +130,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ['dotenv/config'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],

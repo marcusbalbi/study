a47540cf082f7e4db61ae1b7667e098a56a0dc84
Marcus Balbi <balbimarcus@gmail.com>
2022-03-01 11:39:44 -0300

    start data maps

---
+++ books/clojure-for-the-brave-and-true/clojure-noob/src/clojure_noob/datastructures.clj
@@ -0,0 +1,25 @@
+(ns cloure-noob.datastructure)
+
+
+(println "Data Structures")
+
+(println 93)
+(println 1.2)
+(println 1/5)
+
+
+;(println 'Lord Voldemort') invalid
+(println "Lord Voldemort")
+(println "\"He who must not be named\"")
+(def name "Galvão Bueno")
+(println (str "Gooooooooooooooooooool é do Brasil!!!!!!" " - " name))
+
+
+(def first-map { :first-name "Marcus" :last-name "Balbi" })
+
+(def map-function { "string-key" + })
+
+(def nested-map { :name { :first "Marcus" :middle "Vinicius" :last "Balbi" } })
+
+(println map-function)
+

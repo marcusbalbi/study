492e00f6934e8cb61a4ad33bb16b7e619b06f0fe
Marcus Balbi <balbimarcus@gmail.com>
2022-04-04 18:26:18 -0300

    learning comp and memoize

--- books/clojure-for-the-brave-and-true/pure-functions/src/pure_functions/core.clj
+++ books/clojure-for-the-brave-and-true/pure-functions/src/pure_functions/core.clj
@@ -3,7 +3,7 @@
 
 (+ 1 2)
 
-(defn wisdom [words] 
+(defn wisdom [words]
   (str words ", Daniel-san"))
 
 
@@ -34,10 +34,10 @@
 
 great-baby-name
 
-(defn sum 
+(defn sum
   ([vals] (sum vals 0))
   ([vals acc-total]
-   (if (empty? vals) acc-total 
+   (if (empty? vals) acc-total
        (sum (rest vals) (+ (first vals) acc-total)))))
 
 (sum [1 2 3 4 5 6])
@@ -45,10 +45,10 @@ great-baby-name
 (rest [0 1 2 3 4 5])
 
 (defn sum ([vals] (sum vals 0))
-           ([vals acc-total]
-            (if (empty? vals)
-              acc-total
-              (recur (rest vals) (+ (first vals) acc-total)))))
+  ([vals acc-total]
+   (if (empty? vals)
+     acc-total
+     (recur (rest vals) (+ (first vals) acc-total)))))
 
 (sum [2 4 6 8 10])
 
@@ -57,6 +57,78 @@ great-baby-name
 (defn clean [text] (s/replace (s/trim text) #"lol" "LOL"))
 
 (clean "My boa constrictor is so sassy lol!        ")
+
+
+;; comp
+
+((comp inc *) 2 3)
+
+
+(def character
+  {:name "Smooches Mcutes"
+   :attributes {:intelligence 10
+                :strenght 4
+                :dexterity 5}})
+
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strenght :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(c-int character)
+(c-str character)
+(c-dex character)
+
+
+(defn getAttribute [c attr]
+  (get-in c [:attributes attr]))
+
+(defn getAttribute2 [c attr]
+  (attr (:attributes c)))
+
+(defn getIntelligence [c]
+  (:intelligence (:attributes c)))
+
+(getAttribute character :dexterity)
+(getAttribute2 character :strenght)
+
+(getIntelligence character)
+
+(int 25.7)
+
+(defn spell-slots [char]
+  (int (inc (/ (c-int char) 2))))
+
+(spell-slots character)
+
+(def spell-slots-comp (comp int inc #(/ % 2) c-int))
+
+(spell-slots-comp character)
+
+(defn two-comp
+  [f g]
+  (fn [& args]
+    (f (apply g args))))
+
+((two-comp inc *) 2)
+
+
+;; memoize
+
+(defn sleepy-identity [x] (Thread/sleep 1000) x)
+
+
+(sleepy-identity "Mr Fantastico")
+
+(def memo-sleeppy-identity (memoize sleepy-identity))
+
+(map memo-sleeppy-identity 
+     ["Mr memoized6"
+      "Mr memoized6"
+      "Mr memoized6"])
+
+
+
+
 
 
 

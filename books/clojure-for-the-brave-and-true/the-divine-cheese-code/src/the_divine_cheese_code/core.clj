34a6345dd41a63912c4fd2f8f2fa311b346035ed
Marcus Balbi <balbimarcus@gmail.com>
2022-04-16 12:17:02 -0300

    require

--- books/clojure-for-the-brave-and-true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ books/clojure-for-the-brave-and-true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -1,7 +1,41 @@
 (ns the-divine-cheese-code.core
+  ;; way presented in the book
+  ;;(require 'the-divine-cheese-code.visualization.svg)
+  ;;(refer 'the-divine-cheese-code.visualization.svg)
+  ;; way that code editor suggest
+  (:require [the-divine-cheese-code.visualization.svg :as svg])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def heists [
+             {:location "Cologne, Germany"
+              :cheese-name "Archbishop Hildebolds Cheese Pretzel"
+              :lat 50.95
+              :lng 6.97}
+             {:location "Zurich, Switezerland"
+              :cheese-name "The Standard Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat 43.30
+              :lng 5.37}
+             {:location "Zurich, Switezerland"
+              :cheese-name "The Lesser Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Vatican City"
+              :cheese-name "The Cheese of Turin"
+              :lat 41.90
+              :lng 12.45}
+])
+
+(defn test-latlng->point []
+  (svg/latlng->point { :lat 32.88 :lng 10.5}))
+
+(test-latlng->point)
+
+(defn- main [& args]
+  (println (svg/points heists)))
+
+(main)
+
--- books/clojure-for-the-brave-and-true/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ books/clojure-for-the-brave-and-true/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -1,1 +1,13 @@
-(ns the-divine-cheese-code.visualization.svg)
+(ns the-divine-cheese-code.visualization.svg 
+  (:require [clojure.string :as string]))
+
+
+(defn latlng->point 
+  "Convert lat/lng map ti comma-separeted string"
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+
+(defn points 
+  [locations]
+  (string/join " " (map latlng->point locations)))

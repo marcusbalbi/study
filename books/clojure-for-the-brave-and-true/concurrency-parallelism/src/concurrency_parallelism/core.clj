e11f6398ac571e9cb310ebf03cfaa60f2171ce77
Marcus Balbi <balbimarcus@gmail.com>
2022-04-25 22:05:41 -0300

    future and promise

--- books/clojure-for-the-brave-and-true/concurrency-parallelism/src/concurrency_parallelism/core.clj
+++ books/clojure-for-the-brave-and-true/concurrency-parallelism/src/concurrency_parallelism/core.clj
@@ -1,7 +1,7 @@
 (ns concurrency-parallelism.core
   (:gen-class))
 
-(future 
+(future
   (Thread/sleep 1000)
   (println "Hi there one second later!"))
 (println "hi there now!")
@@ -55,6 +55,65 @@
   (doseq [picture my-pictures]
     (future (upload-document picture)
             (force notify))))
+
+
+;;; Promises
+
+(def my-promise (promise))
+(deliver my-promise (+ 1 2))
+@my-promise
+
+
+(def yak-butter-international
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+
+(def butter-than-nothing
+  {:store "Butter Than Nothink"
+   :price 150
+   :smoothness 83})
+
+;; buttter that meets requirements
+(def baby-got-yak
+  {:store "Baby Got Yak"
+   :price 94
+   :smoothness 99})
+
+(defn mock-api-call [result]
+  (Thread/sleep 1000)
+  result)
+
+(defn satisfactory?
+  "If the butter meets our criteria, return the butter, ele return false"
+  [butter]
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
+
+
+(time (some (comp satisfactory? mock-api-call)
+            [yak-butter-international butter-than-nothing baby-got-yak]))
+
+
+(time
+ (let [butter-promise (promise)]
+   (doseq [butter [yak-butter-international butter-than-nothing baby-got-yak]]
+     (future (if-let [satisfactory-butter (satisfactory? (mock-api-call butter))]
+               (deliver butter-promise satisfactory-butter))))
+   ;; (println "And the winner is " @butter-promise) this could block the thread if none is satisfact
+   (println "And the winner is" (deref butter-promise 2000"timed out!"))))
+
+
+(let [ferengi-wisdom-promise (promise)]
+      (future (println "Here is some Ferengi wisdom: " @ferengi-wisdom-promise))
+  (Thread/sleep 100)
+  (deliver ferengi-wisdom-promise "Whisper your way to success")
+  nil)
+
+
+
+
 
 
 

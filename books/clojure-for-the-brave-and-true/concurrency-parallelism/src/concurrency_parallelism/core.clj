0a25fbd1956d9fa0f589beef31f94e4ec7d31b00
Marcus Balbi <balbimarcus@gmail.com>
2022-04-26 22:02:59 -0300

    finish chapter 9

--- books/clojure-for-the-brave-and-true/concurrency-parallelism/src/concurrency_parallelism/core.clj
+++ books/clojure-for-the-brave-and-true/concurrency-parallelism/src/concurrency_parallelism/core.clj
@@ -113,6 +113,40 @@
 
 
 
+(defmacro wait
+  "Sleep  `timeout` seconds before evaluating body"
+  [timeout & body]
+  `(do (Thread/sleep ~timeout) ~@body))
+
+
+(time (let [saying3 (promise)]
+        (future (deliver saying3 (wait 100 "Cheerio!")))
+        @(let [saying2 (promise)]
+           (future (deliver saying2 (wait 400 "Pip Pip!")))
+           @(let [saying1 (promise)]
+              (future (deliver saying1 (wait 200 "Gov na!")))
+              (println @saying1)
+              saying1)
+           (println @saying2)
+           saying2)
+        (println @saying3)
+        saying3))
+
+
+(defmacro enqueue
+  ([q concurrent-promise-name concurrent serialized]
+   `(let [~concurrent-promise-name (promise)]
+      (future (deliver ~concurrent-promise-name ~concurrent))
+      (deref ~q)
+      ~serialized
+      ~concurrent-promise-name))
+  ([concurrent-promise-name concurrent serialized]
+   `(enqueue (future) ~concurrent-promise-name ~concurrent ~serialized)))
+
+(time @(-> (enqueue saying (wait 200 "Gov na!") (println @saying))
+           (enqueue saying (wait 400 "Pip Pip!") (println @saying))
+           (enqueue saying (wait 100 "Cheerio!") (println @saying))))
+
 
 
 
--- books/clojure-for-the-brave-and-true/experimentals/factories/src/factories/core.clj
+++ books/clojure-for-the-brave-and-true/experimentals/factories/src/factories/core.clj
@@ -8,6 +8,17 @@
 
 (conj (car-factory "up!" "blue" 180) {:gas 100})
 
+(println "Ola Mundo")
+
+(java.lang.Integer/parseInt "2000")
+(java.time.Instant/now)
+(Thread/sleep 1000)
+
+(defn greets [nome]
+  (println "Ola " nome))
+
+(greets "Balbi")
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]

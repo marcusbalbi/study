2886db7b4ddd81eff18e394d880572d41b77ad40
Marcus Balbi <balbimarcus@gmail.com>
2022-05-15 11:01:43 -0300

    jvm chapter started

---
+++ books/clojure-for-the-brave-and-true/jvm/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jvm
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ books/clojure-for-the-brave-and-true/phrasebook/PiratePhrases.java
@@ -0,0 +1,8 @@
+public class PiratePhrases
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Shiver me timbers!!!");
+  }
+  
+}

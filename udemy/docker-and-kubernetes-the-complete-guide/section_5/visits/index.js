4151314274ddff98b3e081053585f0ee8c23c1c1
Marcus Balbi <balbimarcus@gmail.com>
2022-05-26 21:01:48 -0300

    start new app visits

---
+++ udemy/docker-and-kubernetes-the-complete-guide/section_5/visits/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const redis = require('redis');
+
+
+const app  = express();
+const client = redis.createClient();
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send(`Number of visits: ${visits}`);
+    client.set('visits', parseInt(visits) + 1);
+  })
+});
+
+app.listen(8081, () => {
+  console.log('listening on ', 8081);
+});

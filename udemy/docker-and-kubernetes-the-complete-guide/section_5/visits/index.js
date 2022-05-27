36c8c9f3da152609bd0e4dbb303fedcce82d0945
Marcus Balbi <balbimarcus@gmail.com>
2022-05-26 21:45:25 -0300

    finish section 5

--- udemy/docker-and-kubernetes-the-complete-guide/section_5/visits/index.js
+++ udemy/docker-and-kubernetes-the-complete-guide/section_5/visits/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const redis = require('redis');
+const { createClient } = require('redis');
 
 
 const app  = express();
-const client = redis.createClient();
-client.set('visits', 0);
+const client = createClient({ url: 'redis://redis-server' });
+client.connect().then(() => {
+  client.set('visits', 0);
+})
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send(`Number of visits: ${visits}`);
-    client.set('visits', parseInt(visits) + 1);
-  })
+app.get('/', async (req, res) => {
+  const visits = await client.get('visits');
+  res.send(`Number of visits: ${visits}`);
+  client.set('visits', parseInt(visits) + 1);
 });
+
+app.get('/exit', (req, res) => {
+  process.exit(0);
+})
+app.get('/exit-failure', (req, res) => {
+  process.exit(1);
+})
 
 app.listen(8081, () => {
   console.log('listening on ', 8081);

65d7a4f22996846996d2206935229760272f1fe4
Marcus Balbi <balbimarcus@gmail.com>
2022-05-28 14:48:05 -0300

    playing with cli

--- clojure/auth-example-hash/src/auth_example_hash/core.clj
+++ clojure/auth-example-hash/src/auth_example_hash/core.clj
@@ -6,7 +6,6 @@
 ;; signin -> email, password -> hash password -> read text compare password and email -> true or false
 ;; bonus add salt
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn -main [& args]
+  (println args))

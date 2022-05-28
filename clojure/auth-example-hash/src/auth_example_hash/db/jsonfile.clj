c5c6e5463bcfb69ec6ceb908def1ea17eb1db252
Marcus Balbi <balbimarcus@gmail.com>
2022-05-28 11:28:46 -0300

    start implemeting jsondb

--- clojure/auth-example-hash/src/auth_example_hash/db/jsonfile.clj
+++ clojure/auth-example-hash/src/auth_example_hash/db/jsonfile.clj
@@ -1,5 +1,19 @@
-(ns auth-example-hash.db.jsonfile)
+(ns auth-example-hash.db.jsonfile
+  (:require [clojure.data.json :as json]))
 
+(defn- fetch-data
+  "Loads a json file in memory and return the data parsed"
+  [filename]
+  (json/read-str (slurp filename) :key-fn keyword))
+
+(fetch-data "db.json")
+
+(defn- persist
+  "save file"
+  [filename data]
+  (spit filename (json/write-str data)))
+
+(persist "db2.json" (fetch-data "db.json"))
 
 (defn save
   "Saves an User into database"

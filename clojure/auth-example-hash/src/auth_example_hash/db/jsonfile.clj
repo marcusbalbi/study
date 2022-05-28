762e6fda557970ba93efe9937f0f9afe1998b42e
Marcus Balbi <balbimarcus@gmail.com>
2022-05-28 13:42:37 -0300

    finish db logic

--- clojure/auth-example-hash/src/auth_example_hash/db/jsonfile.clj
+++ clojure/auth-example-hash/src/auth_example_hash/db/jsonfile.clj
@@ -1,6 +1,7 @@
 (ns auth-example-hash.db.jsonfile
   (:require [clojure.data.json :as json]))
 
+
 (defn- fetch-data
   "Loads a json file in memory and return the data parsed"
   [filename]
@@ -27,8 +28,17 @@
 
 (defn find-by-email
   "Find an User by its email"
-  [filename email])
+  [filename email]
+  (let [data (fetch-data filename)]
+    (first (filter #(= (:email %)
+                       email)
+                   data))))
+
+(comment (find-by-email "db.json" "rambotest@example.com"))
 
 (defn exists?
   "true if the user already exists on database false otherwise"
-  [filename email])
+  [filename email]
+  (if (= (find-by-email filename email) nil) false true))
+
+(comment (exists? "db.json" "rambotest2@example.com"))

8d14161aa8e3fe690fe0da1a331eb1e34f991bf3
Marcus Balbi <balbimarcus@gmail.com>
2022-10-10 18:06:43 -0300

    start app component

--- udemy/advanced-react-and-redux/testing/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hi there', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hi there/i);
-  expect(linkElement).toBeInTheDocument();
-});

be6aa541a94c86d59c21962f37ac19c6acef4c50
Marcus Balbi <balbimarcus@gmail.com>
2023-01-22 11:56:59 -0300

    exercicio 1

---
+++ javascript-masterclass/exercises/create-table.js
@@ -0,0 +1,25 @@
+const readCommand = (sql) => {
+  if (!sql || typeof sql !== 'string') return null;
+  if (!sql.startsWith('create table')) return null;
+
+  const tableName = _extractTableName(sql);
+  const columns = _extractColums(sql);
+
+  return { tableName, columns }
+}
+
+const _extractTableName = (sql) => {
+  const regexCreate = /^create table ([\w]+).*$/;
+  const result = regexCreate.exec(sql);
+  return result && result.length > 0 &&  result[1]
+}
+
+const _extractColums = (sql) => {
+    const regexCreate = /\(([\w]* [\w]*,?)+\)/;
+    const result = regexCreate.exec(sql);
+    if  (!result) return null;
+    return result[0].replace(/(\(|\))/g, '').split(',')
+}
+
+
+module.exports = { readCommand }
---
+++ javascript-masterclass/exercises/create-table.spec.js
@@ -0,0 +1,9 @@
+const { readCommand } = require('./create-table.js')
+describe('read command', () => {
+  test('read command', () => {
+    const sql = `create table author (id number, name string, age number, city string, state string, country string)`;
+    const response = readCommand(sql);
+    expect(response.tableName).toBe('author');
+    expect(response.columns.length).toBe(6);
+  })
+})

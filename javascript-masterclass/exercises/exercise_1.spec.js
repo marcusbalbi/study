e88edebaaa217c199313e1c0dd236abcf7bb1825
Marcus Balbi <balbimarcus@gmail.com>
2023-01-22 12:07:27 -0300

    rename files

--- javascript-masterclass/exercises/exercise_1.spec.js
+++ javascript-masterclass/exercises/exercise_1.spec.js
@@ -1,9 +1,8 @@
-const { readCommand } = require('./create-table.js')
+const { readCommand } = require('./exercise_1.js')
 describe('read command', () => {
   test('read command', () => {
     const sql = `create table author (id number, name string, age number, city string, state string, country string)`;
     const response = readCommand(sql);
-    console.log(response);
     expect(response.tableName).toBe('author');
     expect(response.columns.length).toBe(6);
   })

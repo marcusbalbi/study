a68bf6f8dc5b568ac4dc25379fe771759684424f
Marcus Balbi <balbimarcus@gmail.com>
2023-04-05 21:58:27 -0300

    playing with async

--- books/clojure-applied/examples/src/examples/chapter4/shopping/async_family.clj
+++ books/clojure-applied/examples/src/examples/chapter4/shopping/async_family.clj
@@ -1,10 +1,12 @@
 (ns examples.chapter4.shopping.async-family
-  (:require [examples.chapter4.shopping.store :as store]))
+  (:require [clojure.core.async :refer [<! >!! chan go go-loop]]
+            [examples.chapter4.shopping.store :as store]))
 
 
 (def shopping-list (ref #{}))
 (def assignments (ref #{}))
 (def shopping-cart (ref #{}))
+(def my-kids {:jhon 0})
 
 (defn init []
   (store/init {:eggs 2 :bacon 3 :apples 3 :candy 5 :soda 2
@@ -14,3 +16,61 @@
    (ref-set assignments #{})
    (ref-set shopping-cart #{})))
 
+(defn assignment [child]
+  (get @assignments child))
+
+(defn buy-cand []
+  (dosync
+    (commute shopping-cart conj (store/grab :candy))))
+
+(defn maybe? [fn]
+  (let [chance (rand-int 6)]
+  (when (< 3 chance)
+    (fn))))
+
+(defn dawdle []
+  (let [t (rand-int 5000)]
+    (Thread/sleep t)
+    (maybe? buy-cand)))
+
+(defn collect-assignement [child]
+  (let [item (assignment child)]
+    (dosync
+      (alter shopping-cart conj item)
+      (alter assignments dissoc child)
+      (ensure shopping-list))))
+
+(defn assign-item-to-child [child]
+  (let [item (first @shopping-list)]
+    (dosync
+      (alter assignments assoc child item)
+      (alter shopping-list disj item))))
+
+(defn send-child-for-item [child item q]
+  (println "child " child "is searching for item " item)
+  (dawdle)
+  (collect-assignement child)
+  (>!! q child))
+
+(defn report []
+  (println "Story Inventory" @store/inventory)
+  (println "Shopping list" @shopping-list)
+  (println "Assignments" @assignments)
+  (println "shopping cart" @shopping-cart))
+
+(defn go-shopping []
+  (init)
+  (report)
+  (let [kids (chan 10)]
+    (doseq [k my-kids]
+           (>!! kids k))
+    (go-loop [kid (<! kids)]
+           (if (seq @shopping-list)
+             (do
+               (go (send-child-for-item kid (assign-item-to-child kid) kids))
+               (recur (<! kids)))
+             (do 
+               (println "done shopping")
+               (report))))))
+
+(go-shopping)

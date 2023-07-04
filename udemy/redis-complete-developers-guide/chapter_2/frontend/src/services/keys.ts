fb16f963810ee885788bbe59cc93d18382a6c884
Marcus Balbi <balbimarcus@gmail.com>
2023-07-03 21:30:05 -0300

    users

--- udemy/redis-complete-developers-guide/chapter_2/frontend/src/services/keys.ts
+++ udemy/redis-complete-developers-guide/chapter_2/frontend/src/services/keys.ts
@@ -1,4 +1,7 @@
-
 export const pageCacheKey = (id: string) => {
   return `pagecache#/${id}`
+};
+
+export const usersKey = (userId: string) => {
+	return `users#/${userId}`;
 };
--- udemy/redis-complete-developers-guide/chapter_2/frontend/src/services/queries/users.ts
+++ udemy/redis-complete-developers-guide/chapter_2/frontend/src/services/queries/users.ts
@@ -1,8 +1,35 @@
+import { usersKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+  const key = usersKey(id);
+  const user = await client.hGetAll(key);
+  return deserialize(id, user);
+};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+  const id = genId();
+  const key = usersKey(id);
+  await client.hSet(key, serialize(attrs))
+
+  return id;
+};
+
+const deserialize = (id: string, user: {[key: string]: string}): CreateUserAttrs & { id: string } => {
+	return {
+		username: user.username,
+		password: user.password,
+    id
+	};
+};
+
+const serialize = (user: CreateUserAttrs) => {
+  return {
+    username: user.username,
+    password: user.password
+  }
+}
